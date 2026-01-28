88559bdf70a9d4bfabeea6fd16eb6efd2ab1e32e
Md kaunain raza <2403051050332@paruluniversity.ac.in>
2026-01-28 10:21:50 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,1 +1,15 @@
+export default function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <div className="tags">
+        <span>Java</span>
+        <span>Python</span>
+        <span>Data Structures</span>
+        <span>Git & GitHub</span>
+        <span>React (Basics)</span>
+      </div>
+    </section>
+  );
+}
 
